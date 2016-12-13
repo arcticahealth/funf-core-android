bc278a1b808418a670bf6f4070e98aea8431a1d2
Roman Visintine <roman@visintine.com>
2016-12-12 21:04:07 -0800

    Encrypt s3 uploads

--- funf_v4/src/main/java/edu/mit/media/funf/storage/AmazonS3Archive.java
+++ funf_v4/src/main/java/edu/mit/media/funf/storage/AmazonS3Archive.java
@@ -15,6 +15,7 @@ import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.google.common.util.concurrent.SettableFuture;
 
 import java.io.File;
@@ -82,20 +83,30 @@ public class AmazonS3Archive implements RemoteFileArchive {
         final AmazonS3 s3Client = new AmazonS3Client(credentialsProvider);
         TransferUtility transferUtility = new TransferUtility(s3Client, context.getApplicationContext());
 
-        final SettableFuture<Boolean> future = SettableFuture.create() ;
+        // Tell S3 to use server side encryption using the provided key
+        ObjectMetadata myObjectMetadata = new ObjectMetadata();
+        myObjectMetadata.setSSEKMSKeyId(BuildConfig.S3_KMS_KEY);
+        myObjectMetadata.setSSEAlgorithm(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
+
+        // We'll need the phone number
         TelephonyManager tMgr = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
         String mPhoneNumber = tMgr.getDeviceId();
-        final String key = credentialsProvider.getIdentityId() + "/" + mPhoneNumber + "/" + file.getName();
-        TransferObserver observer = transferUtility.upload(bucketName, key,
-                file);
+
+        // Set our upload url for s3
+        final String fileUrl = credentialsProvider.getIdentityId() + "/" + mPhoneNumber + "/" + file.getName();
+
+        // Set the future... becase we are magicians
+        final SettableFuture<Boolean> future = SettableFuture.create() ;
+
+        TransferObserver observer = transferUtility.upload(bucketName, fileUrl, file, myObjectMetadata);
         observer.setTransferListener(new TransferListener() {
             @Override
             public void onStateChanged(int id, TransferState state) {
                 if (state == TransferState.COMPLETED) {
-                    Log.d(TAG,"upload completed for: " + key);
+                    Log.d(TAG,"upload completed for: " + fileUrl);
                     future.set(true);
                 } else if (state == TransferState.FAILED) {
-                    Log.e(TAG,"upload FAILED for: " + key);
+                    Log.e(TAG,"upload FAILED for: " + fileUrl);
                     future.set(false);
                 }
             }
